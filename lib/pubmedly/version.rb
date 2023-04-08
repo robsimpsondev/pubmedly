37f49f81402a0eea2f38b4ffcc98b6372a9679d9
Rob Simpson <robsimpsondev@gmail.com>
2023-04-08 21:36:39 +0100

    Initial gem scaffold

---
+++ lib/pubmedly/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Pubmedly
+  VERSION = "0.1.0"
+end
---
+++ spec/pubmedly_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pubmedly do
+  it "has a version number" do
+    expect(Pubmedly::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
